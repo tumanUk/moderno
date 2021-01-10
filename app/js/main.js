833d0257381df392c6e35d1a391ffc29513f1cd6
sergeykhomchenko <khomchenko18@gmail.com>
2021-01-10 19:54:07 +0200

    adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -38,29 +38,43 @@ $(function () {
     ],
   });
 
-   // range slider
-   $(".js-range-slider").ionRangeSlider({
+  // range slider
+  $(".js-range-slider").ionRangeSlider({
     type: "double",
     min: 0,
     max: 1500,
     from: 0,
     to: 600,
-        prefix: "$"
-});
+    prefix: "$",
+  });
 
-$('.icon-th-list').on('click', function(){
-  $('.product__item').addClass('list');
-  $('.icon-th-list').addClass('active');
-  $('.icon-th-large').removeClass('active');
-});
+  $(".icon-th-list").on("click", function () {
+    $(".product__item").addClass("list");
+    $(".icon-th-list").addClass("active");
+    $(".icon-th-large").removeClass("active");
+  });
 
-$('.icon-th-large').on('click', function(){
-  $('.product__item').removeClass('list');
-  $('.icon-th-large').addClass('active');
-  $('.icon-th-list').removeClass('active');
+  $(".icon-th-large").on("click", function () {
+    $(".product__item").removeClass("list");
+    $(".icon-th-large").addClass("active");
+    $(".icon-th-list").removeClass("active");
+  });
 
-});
 
-var mixer = mixitup(".products__inner-box");
 
+
+  $(".menu__btn").on("click", function () {
+    $(".menu__list").slideToggle();
+  });
+
+
+
+
+  $(".header__btn-menu").on("click", function () {
+    $(".header__box").toggleClass('active');
+  });
+
+
+
+  var mixer = mixitup(".products__inner-box");
 });
